20bd8b889df2af3684b7f0286a84b83234799111
Andrew <a.oppman@gmail.com>
2023-04-02 15:47:00 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+let multiply = (num1 * num2);
+console.log(multiply);
+
+
+
+let random = 7
+Number.isInteger(random);
+console.log(random);
+
+
+
+const num3 = 16;
+const num4 = 4;
+
+let mod = (num3 / num4);
+console.log(mod);
+
+
+
+
+let max = Math.max(5, 10, 15, 20);
+console.log(max);
+
+
