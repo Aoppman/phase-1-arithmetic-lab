100177c74bc229417aa93810504ae68964730b51
Andrew <a.oppman@gmail.com>
2023-06-27 19:56:35 -0400

    update to index.js

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const num1 = 31;
+/*const num1 = 31;
 const num2 = 2;
 
 let multiply = (num1 * num2);
 console.log(multiply);
 
 
-
 let random = 7
 Number.isInteger(random);
 console.log(random);
-
-
 
 const num3 = 16;
 const num4 = 4;
@@ -19,9 +16,22 @@ let mod = (num3 / num4);
 console.log(mod);
 
 
-
-
 let max = Math.max(5, 10, 15, 20);
 console.log(max);
+*/
+
+let num1 = 15.5
+let num2 = (2 * 2)
+
+const multiply = num1 * num2
+
+let random = 5
+Number.isInteger(random)
 
 
+let num3 = 16;
+let num4 = 6;
+
+const mod = (num3 % num4)
+
+ const max = Math.max(0, 1, 3, 5, 12, 17, 20);
